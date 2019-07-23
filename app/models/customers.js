35c3505e175d1b7937ad29551670c5444a24ee07
buka4chocksy <buka4chocksy@gmail.com>
2019-07-23 17:22:22 +0100

    created customer model

---
+++ app/models/customers.js
@@ -0,0 +1,19 @@
+var mongoose = require ("mongoose");
+var Schema = mongoose.Schema;
+var customersSchema = Schema({
+ name	:{type:String , required:true},
+ email:{type:String , required:true},
+address_1:{type:String , required:true},
+address_2:{type:String , required:true},
+city:{type:String , required:true},
+region:{type:String , required:true},
+postal_code:{type:String , required:true},
+country:{type:String , required:true},
+shipping_region_id:{type: mongoose.SchemaTypes.ObjectId, ref: "shipping"},
+day_phone:{type:String , required:true},
+eve_phone:{type:String , required:true},
+mob_phone:{type:String , required:true},
+credit_card:{type:String , required:true}
+})
+
+module.exports = mongoose.model('customers', customersSchema);
