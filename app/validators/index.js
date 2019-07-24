fe26a6aeb954800f9637e2d4d26fe9b749c4aab7
buka4chocksy <buka4chocksy@gmail.com>
2019-07-24 01:08:58 +0100

    created index joi validator

---
+++ app/validators/index.js
@@ -0,0 +1,11 @@
+var Joi = require('joi');
+module.exports = function validate(data, schema) {
+    return new Promise((resolve, reject) => {
+        Joi.validate(data, schema).then(result => {
+           resolve();
+       }).catch(error => {
+           reject(error.details.map(error => error.message));
+       });
+
+    })
+}
