785d1c5d435a3a2a69b692bc7eecf06864ede414
buka4chocksy <buka4chocksy@gmail.com>
2019-07-24 01:07:17 +0100

    created cutomer model

--- app/models/customers.js
+++ app/models/customers.js
@@ -3,11 +3,12 @@ var Schema = mongoose.Schema;
 var customersSchema = Schema({
  name	:{type:String , required:true},
  email:{type:String , required:true},
+ password:{type:String, required:true},
 address_1:{type:String , required:true},
 address_2:{type:String , required:true},
 city:{type:String , required:true},
 region:{type:String , required:true},
-postal_code:{type:String , required:true},
+postal_code:{type:Number , required:true},
 country:{type:String , required:true},
 shipping_region_id:{type: mongoose.SchemaTypes.ObjectId, ref: "shipping"},
 day_phone:{type:String , required:true},
