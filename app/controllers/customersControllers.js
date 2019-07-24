aa269b20feb3b70437b004ed34925d0f44cbfed6
buka4chocksy <buka4chocksy@gmail.com>
2019-07-24 01:05:10 +0100

    added models for the customer controllers

--- app/controllers/customersControllers.js
+++ app/controllers/customersControllers.js
@@ -1,4 +1,5 @@
 var service = require("../service/customerService");
+var bcrypt = require('bcryptjs');
 
 module.exports = function authController  (){
     this.Register = (req,res)=>{
@@ -26,7 +27,7 @@ module.exports = function authController  (){
     }
 
    this.login = (req, res)=>{
-       
+
     var username = req.body.username
     var password = req.body.password
     service.login(username , password).then(data =>{
@@ -34,5 +35,32 @@ module.exports = function authController  (){
     }).catch(err =>{
         res.status(500).send(err);
     }) 
+   }
+
+   this.getCustomer = (req, res)=>{
+       service.getCustomerById(req.params.id).then(data =>{
+        res.status(200).send(data); 
+    }).catch(err =>{
+        res.status(500).send(err);
+    })
+   }
+
+   this.updateCustomer = (req, res)=>{
+    let hash = bcrypt.hashSync(req.body.password , 10);
+
+       var detail ={
+           name:req.body.name,
+           email:req.body.email,
+           password:hash,
+           day_phone:req.body.dayphone,
+           eve_phone:req.body.evephone,
+           mob_phone:req.body.mobphone
+       }
+       console.log(detail , 'id gotten for current user')
+       service.updateCustomer(req.auth.id , detail).then(data =>{
+        res.status(200).send(data);    
+       }).catch(err =>{
+        res.status(500).send(err);
+    })
    }
 }
