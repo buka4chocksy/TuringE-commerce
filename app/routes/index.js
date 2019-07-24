980c1e73233d77754091ff9a4984915ea2c6b739
buka4chocksy <buka4chocksy@gmail.com>
2019-07-24 16:05:23 +0100

    modified  index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,14 @@
 var shippingRoutes = require('../routes/shippingRoutes');
 var customerRoutes = require('./customersRoutes');
+var departmentRoutes = require('../routes/departmentRoutes');
+var categoriesRoutes = require('../routes/categoriesRoutes');
+var productRoutes = require('../routes/productRoutes');
 module.exports = function(router){
  router.use("/shipping" , shippingRoutes());
  router.use("/customers", customerRoutes());
+ router.use("/departments", departmentRoutes());
+ router.use("/categories", categoriesRoutes());
+ router.use("/products", productRoutes());
 
 
  return router;
